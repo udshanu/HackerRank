969824f4e1b697957ec742b20162b6a24cd5ef0a
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2022-02-20 22:32:55 +0530

    Create Solution.cs

---
+++ Prepare/Algorithms/Warmup/SimpleArraySum/Solution.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int simpleArraySum(List<int> ar)
+    {
+        var array = ar.ToArray();
+        if (array.Length == 0)
+        {
+            return 0;
+        }
+
+        int result = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            result = result + array[i];
+        }
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.simpleArraySum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
