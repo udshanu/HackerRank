dd31e042dcc145351501ac20320498c0d0213c16
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2022-04-03 09:12:41 +0530

    Create ArraysDS.cs

---
+++ Data Structures/Arrays/ArraysDS.cs
@@ -0,0 +1,57 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<int> reverseArray(List<int> a)
+    {
+        var arr = a.ToArray();
+        List<int> output = new List<int>();
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            output.Add(arr[i]);
+        }
+        
+        return output;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> res = Result.reverseArray(arr);
+
+        textWriter.WriteLine(String.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
