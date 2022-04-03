e9db16eb68078013bf3281e7a6176318dcaf7254
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2022-04-03 07:46:43 +0530

    Update Weather Observation Station 8.sql

--- SQL/Weather Observation Station 8/Weather Observation Station 8.sql
+++ SQL/Weather Observation Station 8/Weather Observation Station 8.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY, 1) IN ('a','e','i','o','u') AND RIGHT(CITY, 1) IN ('a','e','i','o','u') OR LEFT(CITY, 1) IN ('a','e','i','o','u') AND RIGHT(CITY, 1) IN ('A','E','I','O','U');
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY, 1) IN ('a','e','i','o','u') AND RIGHT(CITY, 1) IN ('a','e','i','o','u') OR LEFT(CITY, 1) IN ('A','E','I','O','U') AND RIGHT(CITY, 1) IN ('A','E','I','O','U');
